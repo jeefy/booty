21d9458b4f4eed25c094912e5b2b30be5b62b7ee
Jeffrey Sica <me@jeefy.dev>
2023-06-24 14:38:28 -0500

    update pxe library location

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -80,6 +80,6 @@ func DownloadFile(url string) error {
 }
 
 func EnsureDeps() {
-	DownloadFile("http://ftp.us.debian.org/debian/dists/stable/main/installer-amd64/20210731/images/netboot/pxelinux.0")
-	DownloadFile("http://ftp.us.debian.org/debian/dists/stable/main/installer-amd64/20210731/images/netboot/debian-installer/amd64/boot-screens/ldlinux.c32")
+	DownloadFile("http://ftp.us.debian.org/debian/dists/stable/main/installer-amd64/20230607/images/netboot/pxelinux.0")
+	DownloadFile("http://ftp.us.debian.org/debian/dists/stable/main/installer-amd64/20230607/images/netboot/debian-installer/amd64/boot-screens/ldlinux.c32")
 }
