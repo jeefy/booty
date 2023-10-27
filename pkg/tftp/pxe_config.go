65abee29be4abd50f0753e5e787f1d7714c60c78
Jeffrey Sica <me@jeefy.dev>
2023-10-27 14:24:18 -0500

    remove ucore FOR NOW

--- pkg/tftp/pxe_config.go
+++ pkg/tftp/pxe_config.go
@@ -21,16 +21,4 @@ func init() {
 	kernel http://[[server]]/data/flatcar_production_pxe.vmlinuz flatcar.first_boot=1 ignition.config.url=http://[[server]]/ignition.json
 	initrd http://[[server]]/data/flatcar_production_pxe_image.cpio.gz
 	boot`
-
-	PXEConfig["ucore"] = `default ucore
-	prompt 1
-	timeout 5
-	
-	display boot.msg
-	
-	label ucore
-		menu default
-		kernel vmlinuz
-		initrd initrd.img
-		append imageurl=ghcr.io/ublue-os/ucore:stable ignition.config.url=http://[[server]]/ignition.json`
 }
